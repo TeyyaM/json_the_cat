b9c9c0943e379b2a2b346041f25662350438cf25
Tessa Azar MacLean <tessaamac@gmail.com>
2021-03-11 22:24:46 +0000

    Refactored! Fixed things! Added Mocha and Chai!

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,27 @@
 const request = require('request');
-const arg = process.argv[2].slice(0, 3);
-const api = 'https://api.thecatapi.com/v1/breeds/search?q=';
 
-request(api + arg, (error, response, body) => {
-  if (error === null && response.statusCode === 200) {
-    // checks if the breed was valid
-    if (!body[2]) {
-      console.log(`Nothing found! Check the if breed, ${arg} is misspelt!`);
-    } else {
-      // returns the description. Success!
-      console.log(JSON.parse(body)[1].description);
+const fetchBreedDescription = (breedName, callback) => {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(url, (error, response, body) => {
+    if (error) {
+      // checks if the URL was valid
+      callback(error.code, null);
+      return;
     }
-  } else {
-    throw error;
-  }
-});
+
+    const data = JSON.parse(body);
+    if (!data.length) {
+      // checks if the breed was valid
+      callback(`Nothing found! Check the if the breed, ${breedName} is misspelt!`, null);
+      return;
+    }
+
+    const breed = data[0];
+    // returns the description. Success!
+    callback(null, breed.description);
+    return;
+  });
+};
+
+module.exports = { fetchBreedDescription };
 
